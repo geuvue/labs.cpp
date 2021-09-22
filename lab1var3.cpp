8cc88a9761c3145fffdcd39a67cdd40d431914d8
geuvue <90173903+geuvue@users.noreply.github.com>
2021-09-22 11:06:06 +0300

    lab1

---
+++ lab1var3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void)
+
+    {
+        double a, d, S;
+        int n;
+        cout << "enter a" << endl;
+        cin >> a;
+        cout << "enter d" << endl;
+        cin >> d;
+        cout << "enter n" << endl;
+        cin >> n;
+        S = ((2*a+d*(n-1))\2)*n;
+
+        cout << "Sum = " << S << endl;
+        return 0;
+}
